02fefb400939fb0ec273a0bb5e2169e9982cb41f
Jamie Stephens <jamie@courierexpert.co.uk>
2023-05-07 10:23:05 +0100

    Cap5MigrationDist

---
+++ dist/esm/web.js
@@ -0,0 +1,7 @@
+import { WebPlugin } from '@capacitor/core';
+export class DatePickerWeb extends WebPlugin {
+    present(_options) {
+        throw new Error('Method not implemented.');
+    }
+}
+//# sourceMappingURL=web.js.map
---
+++ dist/plugin.cjs.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var core = require('@capacitor/core');
+
+const DatePicker = core.registerPlugin('DatePicker', {
+    web: () => Promise.resolve().then(function () { return web; }).then(m => new m.DatePickerWeb()),
+});
+
+class DatePickerWeb extends core.WebPlugin {
+    present(_options) {
+        throw new Error('Method not implemented.');
+    }
+}
+
+var web = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    DatePickerWeb: DatePickerWeb
+});
+
+exports.DatePicker = DatePicker;
+//# sourceMappingURL=plugin.cjs.js.map
